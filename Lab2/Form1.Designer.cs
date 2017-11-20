290c45d70443da792c0fffa82091fa376c37898a
tmahan95 <tmahan95@users.noreply.github.com>
2017-11-20 13:39:13 -0500

    199 lab 2

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Lab2
+{
+    partial class labForm2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button nameForm1;
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.fullNameTextLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.middleNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.nameForm2 = new System.Windows.Forms.Button();
+            this.nameForm3 = new System.Windows.Forms.Button();
+            this.nameForm4 = new System.Windows.Forms.Button();
+            this.nameForm5 = new System.Windows.Forms.Button();
+            this.nameForm6 = new System.Windows.Forms.Button();
+            nameForm1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameForm1
+            // 
+            nameForm1.Location = new System.Drawing.Point(33, 248);
+            nameForm1.Name = "nameForm1";
+            nameForm1.Size = new System.Drawing.Size(75, 23);
+            nameForm1.TabIndex = 9;
+            nameForm1.Text = "Form 1";
+            nameForm1.UseVisualStyleBackColor = true;
+            nameForm1.Click += new System.EventHandler(this.nameForm1_Click);
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(39, 34);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(58, 13);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First name:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(39, 111);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(59, 13);
+            this.lastNameLabel.TabIndex = 1;
+            this.lastNameLabel.Text = "Last name:";
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.Location = new System.Drawing.Point(38, 74);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(70, 13);
+            this.middleNameLabel.TabIndex = 2;
+            this.middleNameLabel.Text = "Middle name:";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(38, 150);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(72, 13);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Preffered title:";
+            // 
+            // fullNameTextLabel
+            // 
+            this.fullNameTextLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fullNameTextLabel.Location = new System.Drawing.Point(41, 198);
+            this.fullNameTextLabel.Name = "fullNameTextLabel";
+            this.fullNameTextLabel.Size = new System.Drawing.Size(260, 23);
+            this.fullNameTextLabel.TabIndex = 4;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(181, 34);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTextBox.TabIndex = 5;
+            // 
+            // middleNameTextBox
+            // 
+            this.middleNameTextBox.Location = new System.Drawing.Point(181, 74);
+            this.middleNameTextBox.Name = "middleNameTextBox";
+            this.middleNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.middleNameTextBox.TabIndex = 6;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(181, 111);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTextBox.TabIndex = 7;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(181, 150);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(100, 20);
+            this.titleTextBox.TabIndex = 8;
+            // 
+            // nameForm2
+            // 
+            this.nameForm2.Location = new System.Drawing.Point(137, 248);
+            this.nameForm2.Name = "nameForm2";
+            this.nameForm2.Size = new System.Drawing.Size(75, 23);
+            this.nameForm2.TabIndex = 10;
+            this.nameForm2.Text = "Form 2";
+            this.nameForm2.UseVisualStyleBackColor = true;
+            this.nameForm2.Click += new System.EventHandler(this.nameForm2_Click);
+            // 
+            // nameForm3
+            // 
+            this.nameForm3.Location = new System.Drawing.Point(236, 248);
+            this.nameForm3.Name = "nameForm3";
+            this.nameForm3.Size = new System.Drawing.Size(75, 23);
+            this.nameForm3.TabIndex = 11;
+            this.nameForm3.Text = "Form 3";
+            this.nameForm3.UseVisualStyleBackColor = true;
+            this.nameForm3.Click += new System.EventHandler(this.nameForm3_Click);
+            // 
+            // nameForm4
+            // 
+            this.nameForm4.Location = new System.Drawing.Point(33, 293);
+            this.nameForm4.Name = "nameForm4";
+            this.nameForm4.Size = new System.Drawing.Size(75, 23);
+            this.nameForm4.TabIndex = 12;
+            this.nameForm4.Text = "Form 4";
+            this.nameForm4.UseVisualStyleBackColor = true;
+            this.nameForm4.Click += new System.EventHandler(this.nameForm4_Click);
+            // 
+            // nameForm5
+            // 
+            this.nameForm5.Location = new System.Drawing.Point(137, 293);
+            this.nameForm5.Name = "nameForm5";
+            this.nameForm5.Size = new System.Drawing.Size(75, 23);
+            this.nameForm5.TabIndex = 13;
+            this.nameForm5.Text = "Form 5";
+            this.nameForm5.UseVisualStyleBackColor = true;
+            this.nameForm5.Click += new System.EventHandler(this.nameForm5_Click);
+            // 
+            // nameForm6
+            // 
+            this.nameForm6.Location = new System.Drawing.Point(236, 293);
+            this.nameForm6.Name = "nameForm6";
+            this.nameForm6.Size = new System.Drawing.Size(75, 23);
+            this.nameForm6.TabIndex = 14;
+            this.nameForm6.Text = "Form 6";
+            this.nameForm6.UseVisualStyleBackColor = true;
+            this.nameForm6.Click += new System.EventHandler(this.nameForm6_Click);
+            // 
+            // labForm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 341);
+            this.Controls.Add(this.nameForm6);
+            this.Controls.Add(this.nameForm5);
+            this.Controls.Add(this.nameForm4);
+            this.Controls.Add(this.nameForm3);
+            this.Controls.Add(this.nameForm2);
+            this.Controls.Add(nameForm1);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.middleNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.fullNameTextLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.middleNameLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Name = "labForm2";
+            this.Text = "Lab2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label middleNameLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label fullNameTextLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox middleNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Button nameForm2;
+        private System.Windows.Forms.Button nameForm3;
+        private System.Windows.Forms.Button nameForm4;
+        private System.Windows.Forms.Button nameForm5;
+        private System.Windows.Forms.Button nameForm6;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,78 @@
+﻿/*Timothy Mahan
+ * Lab 2
+ * Due: 2/2/16
+ * Course Section: 01
+ * This program takes input from the user and concatenates
+ * it in different ways depending on what button they push.
+ * When they input the information and press a button, the concatenation
+ * is displayed in a Label that lies between the text boxes and the buttons.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class labForm2 : Form
+    {
+        public labForm2()
+        {
+            InitializeComponent();
+        }
+
+        private void nameForm1_Click(object sender, EventArgs e)
+        {
+            String userName; //Declares string variable that will contain the full user name 
+            userName = titleTextBox.Text + " " + firstNameTextBox.Text + " " + middleNameTextBox.Text + " " + lastNameTextBox.Text; //Concatenates the Text property of title, first, middle, and last name text boxes with spaces in between each.
+            fullNameTextLabel.Text = userName; //Enters the text from the string userName into the full name text label.
+        }
+
+        private void nameForm2_Click(object sender, EventArgs e)
+        {
+            string userName; //Declares string variable that will contain the full user name
+            userName = firstNameTextBox.Text + " " + middleNameTextBox.Text + " " + lastNameTextBox.Text; //Concatenates the Text property of first, middle and last name text boxes with spaces in between them
+            fullNameTextLabel.Text = userName; //Enters the text from the string userName into the full name text label.
+        }
+        
+        private void nameForm3_Click(object sender, EventArgs e)
+
+        {
+            string userName; //Declares string variable that will contain the full user name
+            userName = firstNameTextBox.Text + " " + lastNameTextBox.Text; //Concatenates the Text property of first and last name text boxes with spaces in between them
+            fullNameTextLabel.Text = userName; //Enters the text from the string userName into the full name text label.
+        }
+
+        private void nameForm4_Click(object sender, EventArgs e)
+        {
+            string userName; //Declares string variable that will contain the full user name
+            userName = lastNameTextBox.Text + ", " + firstNameTextBox.Text + " " + middleNameTextBox.Text + ", " + titleTextBox.Text; //Concatenates the Text property of last, first, middle, and title text boxes with spaces and commas between them
+            fullNameTextLabel.Text = userName; //Enters the text from the string userName into the full name text label.
+        }
+
+        private void nameForm5_Click(object sender, EventArgs e)
+        {
+            string userName; //Declares string variable that will contain the full user name
+            userName = lastNameTextBox.Text + ", " + firstNameTextBox.Text + " " + middleNameTextBox.Text; //Concatenates the Text property of last, first, and middle name text boxes with spaces and a comma between them.
+            fullNameTextLabel.Text = userName; //Enters the text from the string userName into the full name text label.
+        }
+
+        private void nameForm6_Click(object sender, EventArgs e)
+        {
+            string userName; //Declares string variable that will contain the full user name
+            userName = lastNameTextBox.Text + ", " + firstNameTextBox.Text; //Concatenates the Text property of last and first name test boxes with a space and a comma between them
+            fullNameTextLabel.Text = userName; //Enters the text from the string userName into the full name text label.
+        }
+
+        
+
+        
+    }
+}
